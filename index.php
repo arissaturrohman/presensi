4b424f3e6a74fe22de0956fe2deb4e9ba4bc3649
arissaturrohman <nangarisku@gmail.com>
2022-07-29 11:13:50 +0700

    Login

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include("inc/config.php");
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Blank</title>
+  <title>Aplikasi Absensi SMK Al-Amin Bonang</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
